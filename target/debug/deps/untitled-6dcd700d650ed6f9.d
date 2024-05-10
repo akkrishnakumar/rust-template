57a75aef5e2d5cdb5c002332dfb462364ef03697
Akhil Krishnakumar <35562044+akkrishnakumar@users.noreply.github.com>
2024-05-10 16:05:32 +0200

    Define project structure
    Signed-off-by: Akhil Krishnakumar <akhil.krishnakumar@mendix.com>

---
+++ target/debug/deps/untitled-6dcd700d650ed6f9.d
@@ -0,0 +1,5 @@
+/Users/akhil/Documents/Dev_Playground/Rust/rust-template/target/debug/deps/untitled-6dcd700d650ed6f9: src/main.rs
+
+/Users/akhil/Documents/Dev_Playground/Rust/rust-template/target/debug/deps/untitled-6dcd700d650ed6f9.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/untitled.d
@@ -0,0 +1,1 @@
+/Users/akhil/Documents/Dev_Playground/Rust/rust-template/target/debug/untitled: /Users/akhil/Documents/Dev_Playground/Rust/rust-template/src/main.rs
